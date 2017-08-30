ebc0bf9e35a290e31147453dee10c7d22764a605
James Mwangi <james@dukaconnect.com>
2017-08-30 12:36:31 +0300

    Enhance to handle multiple shopping lists

--- shopping-list/src/App.vue
+++ shopping-list/src/App.vue
@@ -1,38 +1,59 @@
 <template>
   <div id="app" class="container">
-    <h2>{{ title }}</h2>
-    <add-item-component :items="items"></add-item-component>
-    <items-component :items="items"></items-component>
-    <div class="footer">
-      <hr>
-      <change-title-component></change-title-component>
+    <ul class="nav nav-tabs" role="tablist">
+      <li
+        v-bind:class="index===0 ? 'active': ''"
+        v-for="(list, index) in shoppinglists" role="presentation">
+        <a v-bind:href="'#' + list.id" v-bind:aria-controls="list.id" role="tab" data-toggle="tab">{{ list.title }}</a>
+      </li>
+    </ul>
+    <div class="tab-content">
+      <div v-bind:class= "index===0 ? 'active' : ''"
+           v-for="(list,index) in shoppinglists"
+           class="tab-pane" role="tabpane" v-bind:id="list.id">
+        <shopping-list-component
+          v-bind:title="list.title"
+          v-bind:items="list.items"></shopping-list-component>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import AddItemComponent from './components/AddItemComponent.vue'
-  import ItemsComponent from './components/ItemsComponent.vue'
-  import ChangeTitleComponent from './components/ChangeTitleComponent.vue'
+  import ShoppingListComponent from './components/ShoppingListComponent.vue'
 
   export default {
     components: {
-      AddItemComponent,
-      ItemsComponent,
-      ChangeTitleComponent
+      ShoppingListComponent
     },
     data () {
       return {
-        items: [{text: 'Bananas', checked: true}, {text: 'Apples', checked: true}],
-        title: 'My shopping list'
-      }
-    },
-    methods: {
-      addItem (text) {
-        this.items.push({
-          text: text,
-          checked: false
-        })
+        shoppinglists: [
+          {
+            id: 'groceries',
+            title: 'Groceries',
+            items: [{ text: 'Bananas', checked: true },
+              { text: 'Apples', checked: false }]
+          },
+          {
+            id: 'clothes',
+            title: 'Clothes',
+            items: [{ text: 'black dress', checked: false },
+              { text: 'all stars', checked: false }]
+          },
+          {
+            id: 'devices',
+            title: 'Devices',
+            items: [{ text: 'bluetooth speaker', checked: false },
+              { text: 'monitor', checked: false }]
+          },
+          {
+            id: 'household',
+            title: 'House Hold',
+            items: [{ text: 'toothbrush', checked: false },
+              { text: 'soap', checked: false }]
+          }
+        ]
       }
     }
   }
---
+++ shopping-list/src/components/ShoppingListComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <add-item-component :items="items"></add-item-component>
+    <items-component :items="items"></items-component>
+    <div class="footer">
+      <hr>
+      <change-title-component></change-title-component>
+    </div>
+  </div>
+</template>
+
+<script>
+  import AddItemComponent from './AddItemComponent.vue'
+  import ItemsComponent from './ItemsComponent.vue'
+  import ChangeTitleComponent from './ChangeTitleComponent.vue'
+
+  export default {
+    components: {
+      AddItemComponent,
+      ItemsComponent,
+      ChangeTitleComponent
+    },
+    props: ['title', 'items']
+  }
+</script>
+
+<style>
+  .footer {
+    font-size: 0.7em;
+    margin-top: 20vh;
+  }
+</style>
