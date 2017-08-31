99dac8480b860bf5f77ecc9c334c9e1253d519ed
James Mwangi <james@dukaconnect.com>
2017-08-31 14:53:06 +0300

    Page 165

---
+++ simple-store/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <show-greetings-component></show-greetings-component>
+    <change-greeting-component></change-greeting-component>
+  </div>
+</template>
+
+<script>
+  import ShowGreetingsComponent from './components/ShowGreetingsComponent.vue'
+  import ChangeGreetingComponent from './components/ChangeGreetingComponent.vue'
+
+  import store from './vuex/store'
+
+  export default {
+    components: {ShowGreetingsComponent, ChangeGreetingComponent},
+    store
+  }
+
+</script>
+
+<style>
+
+</style>
---
+++ simple-store/src/components/ChangeGreetingComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <input :value="msg" @keyup="changeMsg">
+</template>
+
+<script>
+  export default {
+    computed: {
+      msg() {
+        return this.$store.state.msg
+      }
+    },
+    methods: {
+      changeMsg(ev){
+        this.$store.commit('changeMessage', ev.target.value)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
